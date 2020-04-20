32720bc58e1af75aaf468ae6cf0b71126a6ba395
Matheus Timbó <matheusfild4@hotmail.com>
2020-04-20 01:50:30 -0300

    Customizing transition animation

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionSpecs, CardStyleInterpolators } from '@react-navigation/stack';
 import Home from './Home'
 import Pokedex from './Pokedex'
 import PokemonDetails from './PokemonDetails'
@@ -8,14 +8,21 @@ const Stack = createStackNavigator();
 
 export default function StackNavigator() {
   return (
-      <NavigationContainer>
-        <Stack.Navigator
-            headerMode="none"
-        >
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Pokedex" component={Pokedex} />
-            <Stack.Screen name="PokemonDetails" component={PokemonDetails} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator
+        headerMode="none"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Pokedex" component={Pokedex} />
+        <Stack.Screen name="PokemonDetails" component={PokemonDetails} options={{
+          transitionSpec: {
+            open: TransitionSpecs.TransitionIOSSpec,
+            close: TransitionSpecs.TransitionIOSSpec,
+          },
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
